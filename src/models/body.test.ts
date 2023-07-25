0b511982ce5f8a6c57ff2afc2e70da10d0b9e1ff
freitzzz <1160907@isep.ipp.pt>
2023-07-25 21:14:18 +0100

    refactor: make HttpBody lazy loaded

---
+++ src/models/body.test.ts
@@ -0,0 +1,27 @@
+import { expect, describe, test } from '@jest/globals';
+import { convert, empty, of } from './body';
+
+describe('body', function () {
+    describe('empty', function () {
+        test('returns a lazy value that resolves to undefined', function () {
+            const body = empty();
+
+            const computed = body.get();
+
+            expect(computed).resolves.toBe(undefined);
+        });
+    });
+
+    describe('convert', function () {
+        test('returns a readable stream that pulls all data from the body', function () {
+            const data = 'secret message';
+            const body = of(() => Promise.resolve(data));
+
+            const readable = convert(body);
+            const result = Promise.resolve(readable.getReader().read());
+            const expected = { done: false, value: data };
+
+            expect(result).resolves.toEqual(expected);
+        });
+    });
+});
--- src/models/body.ts
+++ src/models/body.ts
@@ -1,4 +1,75 @@
+import { AsyncLazy } from "../type-utils";
+
+type Text = string;
+type Binary = Blob;
+type JSON = NonNullable<object>;
+type JSONArray = Array<NonNullable<JSON | number | string | boolean>>;
+type Anything<T = object | Binary | JSON | JSONArray | Text | undefined> = T;
+
 /**
  * Types a request/response body as a {@link Buffer}.
  */
-export type HttpBody = Buffer;
+export type HttpBody<T = Anything> = AsyncLazy<T>;
+
+/**
+ * Creates a {@link HttpBody} that resolves an empty(undefined) value.
+ * 
+ * @returns a lazy {@link HttpBody} that resolves an empty promise.
+ */
+export function empty(): HttpBody {
+    return AsyncLazy.async(() => Promise.resolve(undefined));
+}
+
+/**
+ * Creates a {@link HttpBody} in a lazy manner.
+ * 
+ * @returns a lazy {@link HttpBody} that does not resolve until computed.
+ */
+export function of<T = Anything>(value: () => Promise<T>): HttpBody<T> {
+    return AsyncLazy.async(value);
+}
+
+/**
+ * Converts a {@link HttpBody} in a {@link ReadableStream}, by pulling data from the computed body value.
+ * 
+ * @param body - the body to convert
+ * @returns a {@link ReadableStream} which pulls data by computing lazy body value.
+ */
+export function convert<T = Anything>(body: HttpBody<T>): ReadableStream<T> {
+    return new ReadableStream({
+        async start(controller) {
+            try {
+                const computed = await body.get();
+
+                controller.enqueue(computed);
+            } catch (error) {
+                controller.error(error);
+            }
+
+            controller.close();
+        },
+    });
+}
+
+
+// export function fromReadableStream(stream: ReadableStream<Uint8Array>): HttpBody {
+//     return AsyncLazy.async(
+//         async function () {
+//             const reader = stream.getReader();
+//             const chunks = new Array<Uint8Array>();
+
+//             for (; ;) {
+//                 const { value, done } = await reader.read();
+
+//                 if (value) {
+//                     chunks.push(value);
+//                 }
+
+//                 if (done) {
+//                     break;
+//                 }
+//             }
+
+//             return Buffer.concat(chunks);
+//         });
+// } 
