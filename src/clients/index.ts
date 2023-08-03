afa0ac1e5196c8dd5848885327d7bb3866a5b304
freitzzz <1160907@isep.ipp.pt>
2023-08-03 20:25:37 +0100

    feat: implement proxy and relay proxy networking clients

--- src/clients/index.ts
+++ src/clients/index.ts
@@ -1,3 +1,5 @@
 export * from './networking_client';
 export * from './raw_github_networking_client';
 export * from './imgur_networking_client';
+export * from './proxy_networking_client';
+export * from './relay_proxy_networking_client';
---
+++ src/clients/relay_proxy_networking_client.ts
@@ -0,0 +1,65 @@
+import { ProxyNetworkingClient } from './proxy_networking_client';
+import { ProxyNetworkingClientPositionalProperties } from './proxy_networking_client';
+import { ProxyConfiguration, ProxyConfigurationPositionalProperties } from './proxy_networking_client';
+
+/**
+ * Types the needed properties to configure a client that proxies an HTTP request using Relay Proxy API.
+ */
+type RelayProxyNetworkingClientPositionalProperties = {
+	configuration: RelayProxyConfiguration;
+} & ProxyNetworkingClientPositionalProperties;
+
+type RelayProxyConfigurationPositionalProperties = ProxyConfigurationPositionalProperties & {
+	bypassBodyDelete: boolean;
+	bypassExposeHeaders: boolean;
+};
+
+/**
+ * A {@link ProxyNetworkingClient} that proxies HTTP requests using [Relay Proxy API](https://github.com/freitzzz/relay-worker).
+ */
+export class RelayProxyNetworkingClient extends ProxyNetworkingClient {
+	constructor({
+		configuration,
+		fetchClient,
+		timeoutMS,
+		interceptors
+	}: RelayProxyNetworkingClientPositionalProperties) {
+		super({
+			configuration: configuration,
+			fetchClient: fetchClient,
+			timeoutMS: timeoutMS,
+			interceptors: interceptors
+		});
+	}
+}
+
+/**
+ * Types the needed details to configure a proxied request using Relay Proxy API.
+ */
+export class RelayProxyConfiguration extends ProxyConfiguration {
+	constructor({
+		url,
+		client,
+		bypassBodyDelete,
+		bypassExposeHeaders
+	}: RelayProxyConfigurationPositionalProperties) {
+		super({
+			client: client,
+			url: url,
+			onSend: (request) => {
+				const fetchRequest = request.toFetchRequest();
+
+				return request.copyWith({
+					url: url,
+					headers: {
+						...request.headers,
+						'x-relay-url': fetchRequest.url,
+						'x-include-body': `${bypassBodyDelete}`,
+						'x-bypass-expose-headers': `${bypassExposeHeaders}`,
+					},
+				}
+				);
+			}
+		});
+	}
+}
