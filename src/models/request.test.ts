a3dc64660551a914a11729ec314e269ea9d5c6bc
freitzzz <1160907@isep.ipp.pt>
2023-08-03 11:08:00 +0100

    fix: toFetchRequest unit tests

---
+++ src/models/request.test.ts
@@ -0,0 +1,30 @@
+import { expect, describe, test } from '@jest/globals';
+import { HttpRequest } from './request';
+import { of } from './body';
+
+describe('request', function () {
+	describe('toFetchRequest', function () {
+		test('sets fetch request body null if request body is empty', function () {
+			const request = new HttpRequest({
+				url: new URL('https://github.com/web-pacote/networking'),
+				verb: 'get',
+			});
+
+			const fetchRequest = request.toFetchRequest();
+
+			expect(fetchRequest.body).toBeNull();
+		});
+
+		test('sets fetch request body as ReadableStream if request body is not empty', function () {
+			const request = new HttpRequest({
+				url: new URL('https://github.com/web-pacote/networking'),
+				verb: 'post',
+				body: of(() => 'hello world!')
+			});
+
+			const fetchRequest = request.toFetchRequest();
+
+			expect(fetchRequest.body).toBeInstanceOf(ReadableStream<Uint8Array>);
+		});
+	});
+});
