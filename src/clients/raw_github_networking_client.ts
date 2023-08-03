a0ab455ba7ebe70929a55199ec15396155b11716
freitzzz <1160907@isep.ipp.pt>
2023-08-03 19:21:05 +0100

    feat: add possibility to include interceptors for raw github networking client

--- src/clients/raw_github_networking_client.ts
+++ src/clients/raw_github_networking_client.ts
@@ -29,7 +29,7 @@ type GitHubRepository = {
  */
 type RawGitHubNetworkingClientPositionalProperties = {
 	repository: GitHubRepository;
-} & Pick<NetworkingClientPositionalParameters, 'fetchClient' | 'timeoutMS'>;
+} & Pick<NetworkingClientPositionalParameters, 'fetchClient' | 'timeoutMS' | 'interceptors'>;
 
 /**
  * A {@link NetworkingClient} that targets RAW GitHub API.
@@ -38,7 +38,8 @@ export class RawGitHubNetworkingClient extends NetworkingClient {
 	constructor({
 		repository,
 		fetchClient,
-		timeoutMS
+		timeoutMS,
+		interceptors,
 	}: RawGitHubNetworkingClientPositionalProperties) {
 		super({
 			baseUrl: resolveUrl(
@@ -46,7 +47,8 @@ export class RawGitHubNetworkingClient extends NetworkingClient {
 				`${repository.owner}/${repository.repo}/${repository.ref}/`
 			),
 			fetchClient: fetchClient,
-			timeoutMS: timeoutMS
+			timeoutMS: timeoutMS,
+			interceptors: interceptors,
 		});
 	}
 }
