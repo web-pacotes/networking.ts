6a06af2523a6d85d5c4c9a98fe889d8fdfc0300f
freitzzz <1160907@isep.ipp.pt>
2023-08-09 14:07:20 +0100

    feat: set HttpBody as a union of Anything or Lazy<Anything>

--- src/models/body.test.ts
+++ src/models/body.test.ts
@@ -1,14 +1,30 @@
 import { expect, describe, test } from '@jest/globals';
-import { convert, empty, of } from './body';
+import { convert, empty, extract, of } from './body';
 
 describe('body', function () {
 	describe('empty', function () {
-		test('returns a lazy value that resolves to null', function () {
+		test('returns a value that is null', function () {
 			const body = empty();
 
-			const computed = body.get();
+			expect(body).toBe(null);
+		});
+	});
 
-			expect(computed).toBe(null);
+	describe('extract', function () {
+		test('returns the literal body value if not a LazyHttpBody', function () {
+			const body = 'value';
+
+			const value = extract(body);
+
+			expect(value).toBe(body);
+		});
+
+		test('computes and returns the lazy body value if a LazyHttpBody', function () {
+			const body = of(() => 'value');
+
+			const value = extract(body);
+
+			expect(value).toBe('value');
 		});
 	});
 
