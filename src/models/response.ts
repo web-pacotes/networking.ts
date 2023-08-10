44c120f76e4387601d722b6bde497adb188adc35
freitzzz <1160907@isep.ipp.pt>
2023-08-10 17:58:44 +0100

    refactor: remove custom jpeg, image and binary responses

--- src/models/response.ts
+++ src/models/response.ts
@@ -1,4 +1,4 @@
-import { Option, Range } from '../type-utils';
+import { Range } from '../type-utils';
 import { HttpBody, extract, of } from './body';
 import { HttpError } from './errors';
 import { HttpHeaders } from './headers';
@@ -39,31 +39,6 @@ type ServerErrorHttpResponsePositionalProperties = Omit<
 	'stringify'
 > & { statusCode: Range<500, 600>; stringify?: boolean };
 
-type JsonHttpResponsePositionalProperties =
-	SuccessfulHttpResponsePositionalProperties & {
-		mediaType: MediaType.json | MediaType.jsonld;
-	};
-
-type ImageHttpResponsePositionalProperties =
-	SuccessfulHttpResponsePositionalProperties & {
-		mediaType:
-			| MediaType.jpeg
-			| MediaType.png
-			| MediaType.bmp
-			| MediaType.gif
-			| MediaType.svg
-			| MediaType.tiff
-			| MediaType.webp;
-	};
-
-type PlainTextHttpResponsePositionalProperties =
-	SuccessfulHttpResponsePositionalProperties & {
-		mediaType: MediaType.plainText;
-	};
-
-type BinaryHttpResponsePositionalProperties =
-	SuccessfulHttpResponsePositionalProperties & { mediaType: MediaType.binary };
-
 /**
  * Types an HTTP response. Property stringify indicates whether or not the response body
  * should be used when creating a string representation of the response.
@@ -86,15 +61,43 @@ export abstract class HttpResponse {
 	 * @returns a {@link HttpResponse} instance that translates the fetch response.
 	 */
 	static fromFetchResponse(response: Response): HttpResponse {
-		for (const responseType of responseTypesByPriority) {
-			const httpResponse = responseType.tryParseFetchResponse(response);
+		const { statusCode, mediaType, headers } = extractEssential(response);
 
-			if (httpResponse) {
-				return httpResponse;
-			}
+		if (statusCode > 499) {
+			return new ServerErrorHttpResponse({
+				headers: headers,
+				mediaType: mediaType,
+				statusCode: statusCode as Range<500, 600>,
+				body: extractBody(response, mediaType),
+			});
+		} else if (statusCode > 399) {
+			return new ClientErrorHttpResponse({
+				headers: headers,
+				mediaType: mediaType,
+				statusCode: statusCode as Range<400, 500>,
+				body: extractBody(response, mediaType),
+			});
+		} else if (statusCode > 299) {
+			return new RedirectionHttpResponse({
+				headers: headers,
+				mediaType: mediaType,
+				statusCode: statusCode as Range<300, 400>,
+				location: new URL(headers['location']),
+			});
+		} else if (statusCode > 199) {
+			return new SuccessfulHttpResponse({
+				headers: headers,
+				mediaType: mediaType,
+				statusCode: statusCode as Range<200, 300>,
+				body: extractBody(response, mediaType),
+			});
+		} else {
+			return new InformationalHttpResponse({
+				headers: headers,
+				mediaType: mediaType,
+				statusCode: statusCode as Range<100, 200>,
+			});
 		}
-
-		return ServerErrorHttpResponse.tryParseFetchResponse(response);
 	}
 
 	constructor({
@@ -142,11 +145,9 @@ export abstract class HttpResponse {
 	}
 
 	toString(): string {
-		return `${this.constructor.name}(Status Code: ${
-			this.statusCode
-		} | Headers: ${this.headers} | Body: ${
-			this.stringify ? extract(this.body) : '...'
-		})`;
+		return `${this.constructor.name}(Status Code: ${this.statusCode
+			} | Headers: ${this.headers} | Body: ${this.stringify ? extract(this.body) : '...'
+			})`;
 	}
 }
 
@@ -154,20 +155,6 @@ export abstract class HttpResponse {
  * Types an HTTP response that is classified as a informational response (status code: **100**-**199**).
  */
 export class InformationalHttpResponse extends HttpResponse {
-	static tryParseFetchResponse(
-		response: Response
-	): Option<InformationalHttpResponse> {
-		if (response.status >= 200) {
-			return;
-		}
-
-		return new InformationalHttpResponse({
-			...(extractEssential(
-				response
-			) as InformationalHttpResponsePositionalProperties)
-		});
-	}
-
 	constructor({
 		headers,
 		mediaType,
@@ -187,74 +174,6 @@ export class InformationalHttpResponse extends HttpResponse {
  * Types an HTTP response that is classified as a successful response (status code: **200**-**299**).
  */
 export class SuccessfulHttpResponse extends HttpResponse {
-	static tryParseFetchResponse(
-		response: Response
-	): Option<SuccessfulHttpResponse> {
-		if (response.status >= 300) {
-			return;
-		}
-
-		const essential = extractEssential(response);
-		const responseType = this.typeOf(essential.mediaType);
-
-		return responseType.parseFetchResponse(
-			response,
-			essential as Pick<
-				SuccessfulHttpResponsePositionalProperties,
-				'statusCode' | 'headers' | 'mediaType'
-			>
-		);
-	}
-
-	static parseFetchResponse(
-		response: Response,
-		{
-			headers,
-			statusCode,
-			mediaType
-		}: Pick<
-			SuccessfulHttpResponsePositionalProperties,
-			'statusCode' | 'headers' | 'mediaType'
-		>
-	): SuccessfulHttpResponse {
-		return new SuccessfulHttpResponse({
-			headers: headers,
-			statusCode: statusCode,
-			mediaType: mediaType,
-			body: of(() => response.text())
-		});
-	}
-
-	static typeOf(mediaType: MediaType): typeof SuccessfulHttpResponse {
-		switch (mediaType) {
-			case MediaType.avif:
-			case MediaType.bmp:
-			case MediaType.gif:
-			case MediaType.ico:
-			case MediaType.jpeg:
-			case MediaType.png:
-			case MediaType.svg:
-			case MediaType.tiff:
-			case MediaType.webp:
-				return ImageHttpResponse;
-			case MediaType.binary:
-			case MediaType.zip:
-			case MediaType.gzip:
-			case MediaType.pdf:
-				return BinaryHttpResponse;
-			case MediaType.json:
-			case MediaType.jsonld:
-				return JsonHttpResponse;
-			case MediaType.plainText:
-			case MediaType.html:
-			case MediaType.xml:
-			case MediaType.xhtml:
-				return PlainTextHttpResponse;
-			default:
-				return SuccessfulHttpResponse;
-		}
-	}
-
 	constructor({
 		body,
 		headers,
@@ -278,21 +197,6 @@ export class SuccessfulHttpResponse extends HttpResponse {
 export class RedirectionHttpResponse extends HttpResponse {
 	readonly location: URL;
 
-	static tryParseFetchResponse(
-		response: Response
-	): Option<RedirectionHttpResponse> {
-		if (response.status >= 400) {
-			return;
-		}
-
-		const essential = extractEssential(response);
-
-		return new RedirectionHttpResponse({
-			...(essential as RedirectionHttpResponsePositionalProperties),
-			location: new URL(essential.headers['location'])
-		});
-	}
-
 	constructor({
 		headers,
 		mediaType,
@@ -315,21 +219,6 @@ export class RedirectionHttpResponse extends HttpResponse {
  * Types an HTTP response that is classified as a client error response (status code: **400**-**499**).
  */
 export class ClientErrorHttpResponse extends HttpResponse {
-	static tryParseFetchResponse(
-		response: Response
-	): Option<ClientErrorHttpResponse> {
-		if (response.status >= 500) {
-			return;
-		}
-
-		return new ClientErrorHttpResponse({
-			...(extractEssential(
-				response
-			) as ClientErrorHttpResponsePositionalProperties),
-			body: extractBody(response)
-		});
-	}
-
 	constructor({
 		body,
 		headers,
@@ -355,15 +244,6 @@ export class ClientErrorHttpResponse extends HttpResponse {
  * Types an HTTP response that is classified as a server error response (status code: **500**-**599**).
  */
 export class ServerErrorHttpResponse extends HttpResponse {
-	static tryParseFetchResponse(response: Response): ServerErrorHttpResponse {
-		return new ServerErrorHttpResponse({
-			...(extractEssential(
-				response
-			) as ServerErrorHttpResponsePositionalProperties),
-			body: extractBody(response)
-		});
-	}
-
 	constructor({
 		body,
 		headers,
@@ -385,168 +265,6 @@ export class ServerErrorHttpResponse extends HttpResponse {
 	}
 }
 
-/**
- * Types a successful HTTP response which media type is {@link MediaType.json} or {@link MediaType.jsonld}.
- */
-export class JsonHttpResponse extends SuccessfulHttpResponse {
-	static parseFetchResponse(
-		response: Response,
-		{
-			headers,
-			statusCode,
-			mediaType
-		}: Pick<
-			JsonHttpResponsePositionalProperties,
-			'statusCode' | 'headers' | 'mediaType'
-		>
-	): JsonHttpResponse {
-		return new JsonHttpResponse({
-			headers: headers,
-			mediaType: mediaType,
-			statusCode: statusCode,
-			body: of(() => response.json())
-		});
-	}
-
-	constructor({
-		body,
-		headers,
-		mediaType = MediaType.json,
-		statusCode
-	}: JsonHttpResponsePositionalProperties) {
-		super({
-			body: body,
-			headers: headers,
-			mediaType: mediaType,
-			statusCode: statusCode
-		});
-	}
-}
-
-/**
- * Types a successful HTTP response which media type starts with `image/...`
- */
-export class ImageHttpResponse extends SuccessfulHttpResponse {
-	static parseFetchResponse(
-		response: Response,
-		{
-			headers,
-			statusCode,
-			mediaType
-		}: Pick<
-			ImageHttpResponsePositionalProperties,
-			'statusCode' | 'headers' | 'mediaType'
-		>
-	): ImageHttpResponse {
-		return new ImageHttpResponse({
-			headers: headers,
-			mediaType: mediaType,
-			statusCode: statusCode,
-			body: of(() => response.blob())
-		});
-	}
-
-	constructor({
-		body,
-		headers,
-		mediaType,
-		statusCode
-	}: ImageHttpResponsePositionalProperties) {
-		super({
-			body: body,
-			headers: headers,
-			mediaType: mediaType,
-			statusCode: statusCode,
-			stringify: false
-		});
-	}
-}
-
-/**
- * Types an successful HTTP response which media type is {@link MediaType.plainText}.
- */
-export class PlainTextHttpResponse extends SuccessfulHttpResponse {
-	static parseFetchResponse(
-		response: Response,
-		{
-			headers,
-			statusCode,
-			mediaType
-		}: Pick<
-			PlainTextHttpResponsePositionalProperties,
-			'statusCode' | 'headers' | 'mediaType'
-		>
-	): PlainTextHttpResponse {
-		return new PlainTextHttpResponse({
-			headers: headers,
-			mediaType: mediaType,
-			statusCode: statusCode,
-			body: of(() => response.text())
-		});
-	}
-
-	constructor({
-		body,
-		headers,
-		mediaType,
-		statusCode
-	}: PlainTextHttpResponsePositionalProperties) {
-		super({
-			body: body,
-			headers: headers,
-			mediaType: mediaType,
-			statusCode: statusCode
-		});
-	}
-}
-
-/**
- * Types an successful HTTP response which media type is {@link MediaType.binary}.
- */
-export class BinaryHttpResponse extends SuccessfulHttpResponse {
-	static parseFetchResponse(
-		response: Response,
-		{
-			headers,
-			statusCode,
-			mediaType
-		}: Pick<
-			BinaryHttpResponsePositionalProperties,
-			'statusCode' | 'headers' | 'mediaType'
-		>
-	): BinaryHttpResponse {
-		return new BinaryHttpResponse({
-			headers: headers,
-			mediaType: mediaType,
-			statusCode: statusCode,
-			body: of(() => response.blob())
-		});
-	}
-
-	constructor({
-		body,
-		headers,
-		mediaType,
-		statusCode
-	}: BinaryHttpResponsePositionalProperties) {
-		super({
-			body: body,
-			headers: headers,
-			mediaType: mediaType,
-			statusCode: statusCode,
-			stringify: false
-		});
-	}
-}
-
-const responseTypesByPriority = [
-	InformationalHttpResponse,
-	SuccessfulHttpResponse,
-	RedirectionHttpResponse,
-	ClientErrorHttpResponse,
-	ServerErrorHttpResponse
-];
-
 function extractEssential(response: Response) {
 	const headers = Object.fromEntries(response.headers);
 
@@ -557,19 +275,29 @@ function extractEssential(response: Response) {
 	};
 }
 
-function extractBody(response: Response) {
-	const contentType = Object.fromEntries(response.headers)['content-type'];
-	const mediaType = tryParseContentType(contentType);
-
-	const successType = SuccessfulHttpResponse.typeOf(mediaType);
-
-	switch (successType) {
-		case ImageHttpResponse:
-		case BinaryHttpResponse:
+function extractBody(response: Response, mediaType: MediaType) {
+	switch (mediaType) {
+		case MediaType.avif:
+		case MediaType.bmp:
+		case MediaType.gif:
+		case MediaType.ico:
+		case MediaType.jpeg:
+		case MediaType.png:
+		case MediaType.svg:
+		case MediaType.tiff:
+		case MediaType.webp:
+		case MediaType.binary:
+		case MediaType.zip:
+		case MediaType.gzip:
+		case MediaType.pdf:
 			return of(() => response.blob());
-		case JsonHttpResponse:
+		case MediaType.json:
+		case MediaType.jsonld:
 			return of(() => response.json());
-		case PlainTextHttpResponse:
+		case MediaType.plainText:
+		case MediaType.html:
+		case MediaType.xml:
+		case MediaType.xhtml:
 		default:
 			return of(() => response.text());
 	}
