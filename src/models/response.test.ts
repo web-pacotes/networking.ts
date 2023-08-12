9279e52653f9b6d8eb8bb453110c4b0675784865
freitzzz <1160907@isep.ipp.pt>
2023-08-12 11:12:01 +0100

    feat: add type guards for http responses

---
+++ src/models/response.test.ts
@@ -0,0 +1,352 @@
+import { expect, describe, test } from '@jest/globals';
+import { HttpResponse, isBinaryResponse, isClientErrorResponse, isImageResponse, isInformationResponse, isJsonResponse, isPlainTextResponse, isRedirectionResponse, isServerErrorResponse, isSuccessfulResponse } from './response';
+import { MediaType } from './media_type';
+
+describe('response', function () {
+	describe('predicates', function () {
+		describe('type guards', function () {
+			describe('isInformationResponse', function () {
+				test('returns true if argument is instance of InformationHttpResponse', function () {
+					const fetchResponse = <Response>{
+						status: 100,
+						headers: new Headers()
+					};
+
+					const httpResponse = HttpResponse.fromFetchResponse(fetchResponse);
+					const predicate = isInformationResponse(httpResponse);
+
+					expect(predicate).toBeTruthy();
+				});
+
+				test('returns false if argument is not instance of InformationHttpResponse', function () {
+					const fetchResponse = <Response>{
+						status: 200,
+						headers: new Headers()
+					};
+
+					const httpResponse = HttpResponse.fromFetchResponse(fetchResponse);
+					const predicate = isInformationResponse(httpResponse);
+
+					expect(predicate).toBeFalsy();
+				});
+			});
+
+			describe('isSuccessfulResponse', function () {
+				test('returns true if argument is instance of SuccessfulHttpResponse', function () {
+					const fetchResponse = <Response>{
+						status: 200,
+						headers: new Headers()
+					};
+
+					const httpResponse = HttpResponse.fromFetchResponse(fetchResponse);
+					const predicate = isSuccessfulResponse(httpResponse);
+
+					expect(predicate).toBeTruthy();
+				});
+
+				test('returns false if argument is not instance of SuccessfulHttpResponse', function () {
+					const fetchResponse = <Response>{
+						status: 100,
+						headers: new Headers()
+					};
+
+					const httpResponse = HttpResponse.fromFetchResponse(fetchResponse);
+					const predicate = isSuccessfulResponse(httpResponse);
+
+					expect(predicate).toBeFalsy();
+				});
+			});
+
+			describe('isRedirectionResponse', function () {
+				test('returns true if argument is instance of RedirectionHttpResponse', function () {
+					const fetchResponse = <Response>{
+						status: 300,
+						headers: new Headers({ 'location': 'https://github.com/web-pacotes/networking.ts/' })
+					};
+
+					const httpResponse = HttpResponse.fromFetchResponse(fetchResponse);
+					const predicate = isRedirectionResponse(httpResponse);
+
+					expect(predicate).toBeTruthy();
+				});
+
+				test('returns false if argument is not instance of RedirectionHttpResponse', function () {
+					const fetchResponse = <Response>{
+						status: 100,
+						headers: new Headers()
+					};
+
+					const httpResponse = HttpResponse.fromFetchResponse(fetchResponse);
+					const predicate = isRedirectionResponse(httpResponse);
+
+					expect(predicate).toBeFalsy();
+				});
+			});
+
+			describe('isClientErrorResponse', function () {
+				test('returns true if argument is instance of ClientErrorHttpResponse', function () {
+					const fetchResponse = <Response>{
+						status: 400,
+						headers: new Headers()
+					};
+
+					const httpResponse = HttpResponse.fromFetchResponse(fetchResponse);
+					const predicate = isClientErrorResponse(httpResponse);
+
+					expect(predicate).toBeTruthy();
+				});
+
+				test('returns false if argument is not instance of ClientErrorHttpResponse', function () {
+					const fetchResponse = <Response>{
+						status: 100,
+						headers: new Headers()
+					};
+
+					const httpResponse = HttpResponse.fromFetchResponse(fetchResponse);
+					const predicate = isClientErrorResponse(httpResponse);
+
+					expect(predicate).toBeFalsy();
+				});
+			});
+
+			describe('isServerErrorResponse', function () {
+				test('returns true if argument is instance of ServerErrorHttpResponse', function () {
+					const fetchResponse = <Response>{
+						status: 500,
+						headers: new Headers()
+					};
+
+					const httpResponse = HttpResponse.fromFetchResponse(fetchResponse);
+					const predicate = isServerErrorResponse(httpResponse);
+
+					expect(predicate).toBeTruthy();
+				});
+
+				test('returns false if argument is not instance of ServerErrorHttpResponse', function () {
+					const fetchResponse = <Response>{
+						status: 100,
+						headers: new Headers()
+					};
+
+					const httpResponse = HttpResponse.fromFetchResponse(fetchResponse);
+					const predicate = isServerErrorResponse(httpResponse);
+
+					expect(predicate).toBeFalsy();
+				});
+			});
+
+			describe('isBinaryResponse', function () {
+				test('returns true if argument media type is binary', function () {
+					const fetchResponse = <Response>{
+						status: 200,
+						headers: new Headers({ 'content-type': 'application/gzip' })
+					};
+
+					const httpResponse = HttpResponse.fromFetchResponse(fetchResponse);
+					const predicate = isBinaryResponse(httpResponse);
+
+					expect(predicate).toBeTruthy();
+				});
+
+				test('returns false if argument media type is not binary', function () {
+					const fetchResponse = <Response>{
+						status: 200,
+						headers: new Headers({ 'content-type': 'application/json' })
+					};
+
+					const httpResponse = HttpResponse.fromFetchResponse(fetchResponse);
+					const predicate = isBinaryResponse(httpResponse);
+
+					expect(predicate).toBeFalsy();
+				});
+			});
+
+			describe('isImageResponse', function () {
+				test('returns true if argument media type starts with "image"', function () {
+					const mediaTypes = Object.values(MediaType).filter((mt) => mt.startsWith('image/'));
+
+					for (const value of mediaTypes) {
+						const fetchResponse = <Response>{
+							status: 200,
+							headers: new Headers({ 'content-type': value })
+						};
+
+						const httpResponse = HttpResponse.fromFetchResponse(fetchResponse);
+						const predicate = isImageResponse(httpResponse);
+
+						expect(predicate).toBeTruthy();
+					}
+				});
+
+				test('returns false if argument media type does not start with "image"', function () {
+					const mediaTypes = Object.values(MediaType).filter((mt) => !mt.startsWith('image/'));
+
+					for (const value of mediaTypes) {
+						const fetchResponse = <Response>{
+							status: 200,
+							headers: new Headers({ 'content-type': value })
+						};
+
+						const httpResponse = HttpResponse.fromFetchResponse(fetchResponse);
+						const predicate = isImageResponse(httpResponse);
+
+						expect(predicate).toBeFalsy();
+					}
+				});
+			});
+
+			describe('isJsonResponse', function () {
+				test('returns true if argument media type ends with "json"', function () {
+					const mediaTypes = Object.values(MediaType).filter((mt) => mt.endsWith('json'));
+
+					for (const value of mediaTypes) {
+						const fetchResponse = <Response>{
+							status: 200,
+							headers: new Headers({ 'content-type': value })
+						};
+
+						const httpResponse = HttpResponse.fromFetchResponse(fetchResponse);
+						const predicate = isJsonResponse(httpResponse);
+
+						expect(predicate).toBeTruthy();
+					}
+				});
+
+				test('returns false if argument media type does not end with "json"', function () {
+					const mediaTypes = Object.values(MediaType).filter((mt) => !mt.endsWith('json'));
+
+					for (const value of mediaTypes) {
+						const fetchResponse = <Response>{
+							status: 200,
+							headers: new Headers({ 'content-type': value })
+						};
+
+						const httpResponse = HttpResponse.fromFetchResponse(fetchResponse);
+						const predicate = isJsonResponse(httpResponse);
+
+						expect(predicate).toBeFalsy();
+					}
+				});
+			});
+
+			describe('isPlainTextResponse', function () {
+				test('returns true if argument media type is either plain text, javascript, xml or html variant', function () {
+					const mediaTypes = [MediaType.plainText, MediaType.js, MediaType.xml, MediaType.html, MediaType.xhtml];
+
+					for (const value of mediaTypes) {
+						const fetchResponse = <Response>{
+							status: 200,
+							headers: new Headers({ 'content-type': value })
+						};
+
+						const httpResponse = HttpResponse.fromFetchResponse(fetchResponse);
+						const predicate = isPlainTextResponse(httpResponse);
+
+						expect(predicate).toBeTruthy();
+					}
+				});
+
+				test('returns false if argument media type is not either plain text, javascript, xml or html variant', function () {
+					const plainTextMediaTypes = [MediaType.plainText, MediaType.js, MediaType.xml, MediaType.html, MediaType.xhtml];
+					const mediaTypes = Object.values(MediaType).filter((mt) => !plainTextMediaTypes.includes(mt));
+
+					for (const value of mediaTypes) {
+						const fetchResponse = <Response>{
+							status: 200,
+							headers: new Headers({ 'content-type': value })
+						};
+
+						const httpResponse = HttpResponse.fromFetchResponse(fetchResponse);
+						const predicate = isPlainTextResponse(httpResponse);
+
+						expect(predicate).toBeFalsy();
+					}
+				});
+			});
+		});
+
+		describe('ok', function () {
+			test('returns true response is either of type informational, successful or redirection', function () {
+				const responses = [100, 200, 300].map((sc) => HttpResponse.fromFetchResponse(<Response>{
+					status: sc,
+					headers: new Headers({ location: 'https://github.com/web-pacotes/networking.ts/' })
+				}));
+
+				for (const value of responses) {
+					const predicate = value.ok();
+
+					expect(predicate).toBeTruthy();
+				}
+			});
+
+			test('returns false response is not either of type informational, successful or redirection', function () {
+				const responses = [400, 500].map((sc) => HttpResponse.fromFetchResponse(<Response>{
+					status: sc,
+					headers: new Headers({ location: 'https://github.com/web-pacotes/networking.ts/' })
+				}));
+
+				for (const value of responses) {
+					const predicate = value.ok();
+
+					expect(predicate).toBeFalsy();
+				}
+			});
+		});
+
+		describe('not ok', function () {
+			test('returns true response is either of type client error, or server error', function () {
+				const responses = [400, 500].map((sc) => HttpResponse.fromFetchResponse(<Response>{
+					status: sc,
+					headers: new Headers()
+				}));
+
+				for (const value of responses) {
+					const predicate = value.notOk();
+
+					expect(predicate).toBeTruthy();
+				}
+			});
+
+			test('returns false response is not either of type client error, or server error', function () {
+				const responses = [100, 200, 300].map((sc) => HttpResponse.fromFetchResponse(<Response>{
+					status: sc,
+					headers: new Headers({ location: 'https://github.com/web-pacotes/networking.ts/' })
+				}));
+
+				for (const value of responses) {
+					const predicate = value.notOk();
+
+					expect(predicate).toBeFalsy();
+				}
+			});
+		});
+
+		describe('redirection', function () {
+			test('returns true response is of type redirection', function () {
+				const responses = [300].map((sc) => HttpResponse.fromFetchResponse(<Response>{
+					status: sc,
+					headers: new Headers({ location: 'https://github.com/web-pacotes/networking.ts/' })
+				}));
+
+				for (const value of responses) {
+					const predicate = value.redirection();
+
+					expect(predicate).toBeTruthy();
+				}
+			});
+
+			test('returns false response is not of type redirection', function () {
+				const responses = [100, 200, 400, 500].map((sc) => HttpResponse.fromFetchResponse(<Response>{
+					status: sc,
+					headers: new Headers()
+				}));
+
+				for (const value of responses) {
+					const predicate = value.redirection();
+
+					expect(predicate).toBeFalsy();
+				}
+			});
+		});
+	});
+});
--- src/models/response.ts
+++ src/models/response.ts
@@ -1,5 +1,5 @@
 import { Range } from '../type-utils';
-import { HttpBody, extract, of } from './body';
+import { Binary, HttpBody, JSON, JSONArray, extract, of } from './body';
 import { HttpError } from './errors';
 import { HttpHeaders } from './headers';
 import { MediaType, tryParseContentType } from './media_type';
@@ -39,6 +39,11 @@ type ServerErrorHttpResponsePositionalProperties = Omit<
 	'stringify'
 > & { statusCode: Range<500, 600>; stringify?: boolean };
 
+type BinaryHttpResponse = Omit<HttpResponse, 'body'> & { body: HttpBody<Binary> };
+type PlainTextHttpResponse = Omit<HttpResponse, 'body'> & { body: HttpBody<Text> };
+type JsonHttpResponse = Omit<HttpResponse, 'body'> & { body: HttpBody<JSON | JSONArray> };
+type ImageHttpResponse = BinaryHttpResponse;
+
 /**
  * Types an HTTP response. Property stringify indicates whether or not the response body
  * should be used when creating a string representation of the response.
@@ -52,7 +57,7 @@ export abstract class HttpResponse {
 
 	readonly headers: HttpHeaders;
 
-	private stringify: boolean;
+	readonly stringify: boolean;
 
 	/**
 	 * Converts a fetch {@link Response} in a {@link HttpResponse}.
@@ -130,8 +135,8 @@ export abstract class HttpResponse {
 	 */
 	notOk(): boolean {
 		return (
-			this instanceof ClientErrorHttpResponse ||
-			this instanceof ServerErrorHttpResponse
+			isClientErrorResponse(this) ||
+			isServerErrorResponse(this)
 		);
 	}
 
@@ -140,8 +145,8 @@ export abstract class HttpResponse {
 	 *
 	 * @returns true if response type matches {@link RedirectionHttpResponse}.
 	 */
-	redirection(): boolean {
-		return this instanceof RedirectionHttpResponse;
+	redirection(): this is RedirectionHttpResponse {
+		return isRedirectionResponse(this);
 	}
 
 	toString(): string {
@@ -265,6 +270,144 @@ export class ServerErrorHttpResponse extends HttpResponse {
 	}
 }
 
+/**
+ * Checks if a {@link HttpResponse} is an instance of {@link InformationalHttpResponse}, providing a 
+ * type guard for that type.
+ * 
+ * @param response - the response instance being checked
+ * @returns boolean true if the response is an instance of {@link InformationalHttpResponse}, false otherwise.
+ */
+export function isInformationResponse(response: HttpResponse): response is InformationalHttpResponse {
+	return response instanceof InformationalHttpResponse;
+}
+
+/**
+ * Checks if a {@link HttpResponse} is an instance of {@link SuccessfulHttpResponse}, providing a 
+ * type guard for that type.
+ * 
+ * @param response - the response instance being checked
+ * @returns boolean true if the response is an instance of {@link SuccessfulHttpResponse}, false otherwise.
+ */
+export function isSuccessfulResponse(response: HttpResponse): response is SuccessfulHttpResponse {
+	return response instanceof SuccessfulHttpResponse;
+}
+
+/**
+ * Checks if a {@link HttpResponse} is an instance of {@link RedirectionHttpResponse}, providing a 
+ * type guard for that type.
+ * 
+ * @param response - the response instance being checked
+ * @returns boolean true if the response is an instance of {@link RedirectionHttpResponse}, false otherwise.
+ */
+export function isRedirectionResponse(response: HttpResponse): response is RedirectionHttpResponse {
+	return response instanceof RedirectionHttpResponse;
+}
+
+/**
+ * Checks if a {@link HttpResponse} is an instance of {@link ClientErrorHttpResponse}, providing a 
+ * type guard for that type.
+ * 
+ * @param response - the response instance being checked
+ * @returns boolean true if the response is an instance of {@link ClientErrorHttpResponse}, false otherwise.
+ */
+export function isClientErrorResponse(response: HttpResponse): response is ClientErrorHttpResponse {
+	return response instanceof ClientErrorHttpResponse;
+}
+
+/**
+ * Checks if a {@link HttpResponse} is an instance of {@link ServerErrorHttpResponse}, providing a 
+ * type guard for that type.
+ * 
+ * @param response - the response instance being checked
+ * @returns boolean true if the response is an instance of {@link ServerErrorHttpResponse}, false otherwise.
+ */
+export function isServerErrorResponse(response: HttpResponse): response is ServerErrorHttpResponse {
+	return response instanceof ServerErrorHttpResponse;
+}
+
+/**
+ * Checks if a {@link HttpResponse} is an instance of {@link BinaryHttpResponse}, providing a 
+ * type guard for that type.
+ * 
+ * @param response - the response instance being checked
+ * @returns boolean true if the response is an instance of {@link BinaryHttpResponse}, false otherwise.
+ */
+export function isBinaryResponse(response: HttpResponse): response is BinaryHttpResponse {
+	return isBinaryMediaType(response.mediaType);
+}
+
+/**
+ * Checks if a {@link HttpResponse} is an instance of {@link JsonHttpResponse}, providing a 
+ * type guard for that type.
+ * 
+ * @param response - the response instance being checked
+ * @returns boolean true if the response is an instance of {@link JsonHttpResponse}, false otherwise.
+ */
+export function isJsonResponse(response: HttpResponse): response is JsonHttpResponse {
+	return isJsonMediaType(response.mediaType);
+}
+
+/**
+ * Checks if a {@link HttpResponse} is an instance of {@link PlainTextHttpResponse}, providing a 
+ * type guard for that type.
+ * 
+ * @param response - the response instance being checked
+ * @returns boolean true if the response is an instance of {@link PlainTextHttpResponse}, false otherwise.
+ */
+export function isPlainTextResponse(response: HttpResponse): response is PlainTextHttpResponse {
+	return isPlainTextMediaType(response.mediaType);
+}
+
+/**
+ * Checks if a {@link HttpResponse} is an instance of {@link ImageHttpResponse}, providing a 
+ * type guard for that type.
+ * 
+ * @param response - the response instance being checked
+ * @returns boolean true if the response is an instance of {@link ImageHttpResponse}, false otherwise.
+ */
+export function isImageResponse(response: HttpResponse): response is ImageHttpResponse {
+	return isImageMediaType(response.mediaType);
+}
+
+function isBinaryMediaType(mediaType: MediaType): boolean {
+	switch (mediaType) {
+		case MediaType.binary:
+		case MediaType.zip:
+		case MediaType.gzip:
+		case MediaType.pdf:
+			return true;
+		default:
+			return isImageMediaType(mediaType);
+	}
+}
+
+function isJsonMediaType(mediaType: MediaType): boolean {
+	switch (mediaType) {
+		case MediaType.json:
+		case MediaType.jsonld:
+			return true;
+		default:
+			return false;
+	}
+}
+
+function isPlainTextMediaType(mediaType: MediaType): boolean {
+	switch (mediaType) {
+		case MediaType.html:
+		case MediaType.js:
+		case MediaType.plainText:
+		case MediaType.xhtml:
+		case MediaType.xml:
+			return true;
+		default:
+			return false;
+	}
+}
+
+function isImageMediaType(mediaType: MediaType): boolean {
+	return mediaType.startsWith('image/');
+}
+
 function extractEssential(response: Response) {
 	const headers = Object.fromEntries(response.headers);
 
@@ -276,29 +419,11 @@ function extractEssential(response: Response) {
 }
 
 function extractBody(response: Response, mediaType: MediaType) {
-	switch (mediaType) {
-		case MediaType.avif:
-		case MediaType.bmp:
-		case MediaType.gif:
-		case MediaType.ico:
-		case MediaType.jpeg:
-		case MediaType.png:
-		case MediaType.svg:
-		case MediaType.tiff:
-		case MediaType.webp:
-		case MediaType.binary:
-		case MediaType.zip:
-		case MediaType.gzip:
-		case MediaType.pdf:
-			return of(() => response.blob());
-		case MediaType.json:
-		case MediaType.jsonld:
-			return of(() => response.json());
-		case MediaType.plainText:
-		case MediaType.html:
-		case MediaType.xml:
-		case MediaType.xhtml:
-		default:
-			return of(() => response.text());
+	if (isJsonMediaType(mediaType)) {
+		return of(() => response.json());
+	} else if (isBinaryMediaType(mediaType)) {
+		return of(() => response.blob());
+	} else {
+		return of(() => response.text());
 	}
 }
